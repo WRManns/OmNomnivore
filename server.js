48d8d68ce5aab4ec2430a7dc0eb9b8ce112d02d9
Ryan Manns <w.r.manns@outlook.com>
2021-07-17 10:59:05 -0400

    added connection and server code and dependencies in packagejson

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const sequelize = require("./client/src/config/connection")
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the MySQL DB
+sequelize.sync({ force: false}).then(() => {
+    app.listen(PORT, () => console.log('Now Listening'));
+  });
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
