1f9343fb6c681792cf3ecdbed2e6ce46634d26df
Ryan Manns <w.r.manns@outlook.com>
2021-07-20 19:47:24 -0400

    bootstrap added to index/.js reactstrap works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import NavXample from "./components/Navbar/Navbar";
 import Example from "./components/Card/Card"
-import Wrapper from "./components/Wrapper";
+import Wrapper from "./components/Wrapper/index";
 import "./App.css";
 
 function App() {
