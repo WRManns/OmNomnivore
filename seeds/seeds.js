2b5786b90484d5209a54938bd0b98bed99bf1d08
Traci <traci.face@gmail.com>
2021-07-27 21:15:47 -0400

    seeds edit

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,24 +1,18 @@
 const sequelize = require('../config/connection');
-const { User, Work } = require('../models');
+const { loginModel } = require('../models');
 
 const userData = require('./user-seeds.json');
-const workData = require('./work-seeds.json');
 
 const seedDatabase = async() => {
     await sequelize.sync({ force: true });
 
-    const users = await User.bulkCreate(userData, {
+    const users = await loginModel.bulkCreate(userData, {
         individualHooks: true,
         returning: true,
     });
 
-    for (const work of workData) {
-        await Work.create({
-            ...work,
-        });
     }
 
     process.exit(0);
-};
 
 seedDatabase();
