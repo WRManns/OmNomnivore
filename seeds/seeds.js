fa813a5b0728b03bcb58e6b002b670936856d9ff
noahhelton98 <noahhelton98@gmail.com>
2021-07-29 19:48:36 -0400

    fixed seeding and db stuff

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,22 +1,15 @@
-const sequelize = require('../config/connection');
-const { loginModel } = require('../models');
+const sequelize = require('../config/connection.js');
+const User = require('../models/loginModel.js');
 
 const userData = require('./user-seeds.json');
 
 const seedDatabase = async() => {
     await sequelize.sync({ force: true });
 
-    const users = await loginModel.bulkCreate(userData, {
+    const users = await User.bulkCreate(userData, {
         individualHooks: true,
         returning: true,
     });
-
-    // for (const work of workData) {
-    //     await Work.create({
-    //         ...work,
-    //     });
-    // }
-
     process.exit(0);
 };
 
