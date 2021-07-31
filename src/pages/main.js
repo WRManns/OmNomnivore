8b05902748adb249140cc39889b6dd597ce7546c
Emily Warner <warneremily32@gmail.com>
2021-07-31 13:03:26 -0400

    main

--- src/pages/main.js
+++ src/pages/main.js
@@ -10,33 +10,14 @@ import SignIn from "../components/Forms/signin.js"
 function Main() {
     return(
         <div className="container">
-<<<<<<< HEAD
+
             
             <Intro/>
             <SignIn/>
             <RegisterForm/>
             
             
-            
 
-            
-=======
-            <Router>
-            </Router>
-            <Route/>
-<<<<<<< HEAD
-            <Switch/>
-=======
-            <Switch>
-            </Switch>
-            <Welcome/>
-            <LogBtn/>
-            
-
-            
->>>>>>> 693e25f8ed1938c1552aec831dd3da47c3894d19
-            <Register/>
->>>>>>> c99f560fc6a5a76daec8e38a08211cc88ccc5139
 
         </div>
 
