78678b3d26722eac36d4d19b93d5d683618d9e61
Ryan Manns <w.r.manns@outlook.com>
2021-08-04 19:29:18 -0400

    most recent commit

--- client/src/components/Card/MenuCard.js
+++ client/src/components/Card/MenuCard.js
@@ -1,43 +1,90 @@
 import React, { useEffect, useState } from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, } from 'reactstrap';
-import {List, ListItem} from "../List"
+import { Button, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, } from 'reactstrap';
+// import "./style.css";
+/* import AutoCompleteRecipe from './recipeAutoAPI'
+import SearchSimilarFoods from './recipeSearchAPI' */
+import {List, ListItem} from "../List/index"
+import { Link } from "react-router-dom";
 
+const MenuList = (props) => {
+  useEffect(() => {
+    console.log(props.menuItems)
+  }, [])
 
-  function MenuItems(props) {
-  
   return (
       <Card>
-        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
         <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.
-          {props.restaurants.length ? (
+          {/* <CardTitle tag="h5">Card title</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle> */}
+          <div>
+          {props.menuItems ? (
               <List>
-                {props.restaurants.map(test => {
-                  console.log(test)
-                  return (
+                {props.menuItems.map(test => {
+                  test.map(item => {
+                    console.log(item)
+                    return (
                     
-                  <ListItem key= {test.restaurant_id}>
-                    <p>
-                      <a href={test.restaurant_website}>
-                      {test.restaurant_name}                          
-                      </a> 
-                    </p>
-                    <p> {test.restaurant_phone} </p>
-                  </ListItem>
-                  );
+                      <ListItem key= {item.name}>
+                        <p>    
+                          {item.name}                          
+                        </p>
+                        <p> {item.description} </p>
+    
+                      </ListItem>
+                     );
+                  })
+              
+               
                 })}
               </List>
+            
             ) : (
-              <h3>No Results to Display</h3>
+              <p>No Results to Display</p>
             )} 
           
-          </CardText>
+        </div>
+
+          
+       
         </CardBody>
       </Card>
     
   );
-}
+            
+// } else{
 
-export default MenuItems;
+//   return (
+//     <Card>
+//         <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+//         <CardBody>
+//           <CardTitle tag="h5">Card title</CardTitle>
+//           <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+//           <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.
+//           {props.recipesObj.length ? (
+//               <List>
+//                 {props.recipesObj.map(test => {
+//                   return (
+//                     <ListItem key= {test.recipesObj.id}>
+//                     <p>
+//                       <a href={test.recipesObj.sourceUrl}>
+//                       {test.recipesObj.title}                          
+//                       </a> 
+                      
+//                     </p>
+//                     {/* <p> {test.restaurant_phone}  </p> */}
+//                     </ListItem>
+//                   );
+//                 })}
+//               </List>
+//             ) : (
+//               <h3>No Results to Display</h3>
+//             )} 
+          
+//           </CardText>
+//         </CardBody>
+//       </Card>
+//   );
+// }
+  }
+
+export default MenuList;
