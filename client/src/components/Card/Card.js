b9a62c2b5ce51d148cfe614fb9cb8102698781a6
Tommy K <t.kuntz76@gmail.com>
2021-08-04 17:23:08 -0400

    tom

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -46,7 +46,7 @@ import { Link } from "react-router-dom";
       <Card>
         <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
         <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
+          <CardTitle tag="h5">Bon Apetite</CardTitle>
           <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
           <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.
           {props.restaurants.length ? (
